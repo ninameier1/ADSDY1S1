982541051f6748f98eb5d08c9f28b654f820736a
ninameier1 <s1081087@student.windesheim.nl>
2024-10-12 11:45:46 +0200

    12-10 Design update homepage

--- views/home.view.php
+++ views/home.view.php
@@ -1,29 +1,23 @@
 <?php include "./views/layout/head.php"?>
 
+<link rel="stylesheet" href="./views/css/custom.css">
+
 <body id="body">
     <main>
-        <link rel="stylesheet" href="./views/css/custom.css">
         <div class="menu">
+        <img src="./views/css/assets/white.png" class="title">
+        <p class="hometext">
+            STUDENT SOFTWARE DEVELOPMENT WINDESHEIM FLEVOLAND
+        </p>
             <a href="/projects">
-                <img src="./views/css/assets/projectsbl.png" class="menulinks" id="projectswh">
+                <img src="./views/css/assets/projectswh.png" class="menulinks" id="projectswh">
             </a>
             <a href="/about">
-                <img src="./views/css/assets/ABOUTbl.png" class="menulinks" id="aboutwh">
+                <img src="./views/css/assets/ABOUTwh.png" class="menulinks" id="aboutwh">
             </a>
             <a href="/contact">
-                <img src="./views/css/assets/contactbl.png" class="menulinks" id="contactwh">
+                <img src="./views/css/assets/contactwh.png" class="menulinks" id="contactwh">
             </a>
-        </div>
-        <div class="section" id="section">
-            <p class="text" id="name">
-                Nina Meier
-            </p>
-            <p class="text">
-                Student Software Development
-            </p>
-            <p class="text">
-                Windesheim Flevoland
-            </p>
         </div>
     </main>
 
--- views/layout/header.php
+++ views/layout/header.php
@@ -1,20 +1,20 @@
 <body id="body">
-    <header id="header">
-        <button>
-            Toggle Dark Mode
-        </button>
-        <nav class="nav1">
-            <a href="/">
-                <img src="./views/css/assets/homebl.png" class="navlinks">
-            </a>
-            <a href="/projects">
-                <img src="./views/css/assets/projectscc.png" class="navlinks">
-            </a>
-            <a href="/about">
-                <img src="./views/css/assets/ABOUTbl.png" class="navlinks">
-            </a>
-            <a href="/contact">
-                <img src="./views/css/assets/contactcc.png" class="navlinks">
-            </a>
-        </nav>
-    </header>
+<header id="header">
+    <button>
+        Toggle Dark Mode
+    </button>
+    <nav class="nav1" id="nav">
+        <a href="/">
+            <img src="./views/CSS/assets/homewh.png" class="navlinks">
+        </a>
+        <a href="/projects">
+            <img src="<?php echo ($title == 'Projects')?'./views/css/assets/projectsbl.png':'./views/css/assets/projectswh.png';?>" class="navlinks">
+        </a>
+        <a href="/about">
+            <img src="<?php echo ($title == 'About')?'./views/css/assets/ABOUTbl.png':'./views/css/assets/ABOUTwh.png';?>" class="navlinks">
+        </a>
+        <a href="/contact">
+            <img src="<?php echo ($title == 'Contact')?'./views/css/assets/contactbl.png':'./views/css/assets/contactwh.png';?>" class="navlinks">
+        </a>
+    </nav>
+</header>
